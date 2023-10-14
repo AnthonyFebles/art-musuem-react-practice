da7848e3e2f2650363c5b4dacf5d3fcc5f08a232
Anthony Febles <anthonyfebles21@outlook.com>
2023-10-14 10:04:05 -0400

    phase 2 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import harvardArt from "./data/harvardArt"; 
 import GalleryNavigation from "./components/GalleryNavigation";
-import GalleryView from "./components/GalleryNavigation/GalleryView";
+import GalleryView from "./components/GalleryView";
 import {ReactRouter, Route} from 'react-router-dom'
 
 function App() {
-  console.log(harvardArt);
+  //console.log(harvardArt);
   return (
 		<div className="page-wrapper">
 			<GalleryNavigation galleries={harvardArt.records} />
-			<Route path="/galleries/:galleryId"></Route>
+			<Route path="/galleries/:galleryId(\d+)">
+				<GalleryView galleries={harvardArt.records}/>
+			</Route>
 		</div>
 	);
 }
---
+++ src/components/GalleryView/index.js
@@ -0,0 +1,20 @@
+import { useParams } from 'react-router-dom'
+import './GalleryView.css'
+
+const GalleryView = ({galleries}) => {
+    const { galleryId } = useParams()
+    //console.log(galleryId)
+    //console.log("HELLLOOOOOOO", galleries)
+    const currGallery = galleries.find(gallery => gallery.id ===parseInt(galleryId) )
+
+    console.log(currGallery)
+
+    return (
+			<div>
+                <h2>{currGallery.name}</h2>
+				<h3>Hello From GalleryView</h3>
+			</div>
+		);
+}
+
+export default GalleryView
